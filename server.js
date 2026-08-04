7ed1d271ce8f87890cc6fb579a03f05139c29ea4
MinhhQuann1506 <167730243+MinhhQuann1506@users.noreply.github.com>
2026-08-04 16:41:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const express = require('express');
 const path = require('path');
 
-// Phục vụ file tĩnh
-app.use(express.static(path.join(__dirname, '.')));
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Route trang chủ
+// Phục vụ file tĩnh (CSS, JS, Images, assets)
+app.use(express.static(path.join(__dirname, '.')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Gwo Dyi Duty VN Web App running at http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
